d51581a17b9dc959b4df4f9e8e06e30af1651819
Breno Pimenta <brenocsp@gmail.com>
2022-02-12 02:24:50 -0300

    uploading report on raw data

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import sys
 import time
 
+import pandas as pd
+import pandas_profiling
+
 def main():
     startTime = time.time()
 
-    ## CODE
+    with open(sys.argv[1], 'r') as f:
+        ratings = pd.read_json(f, lines=True)
+
+    with open(sys.argv[2], 'r') as f:
+        content = pd.read_json(f, lines=True)
+        
+    with open(sys.argv[3], 'r') as f:
+        targets = pd.read_csv(f, sep=',', engine='python')
+
+    profile = pandas_profiling.ProfileReport(content)
+    profile.to_file('./results/profiler_raw_data.html')
     
     print("Time: %s seconds " % (time.time() - startTime))
 
